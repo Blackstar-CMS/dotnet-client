de3a190ae6b75735a9756f5e7f03cf442ebc07cd
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-07-20 20:10:16 +1000

    A .NET Core-compatible (netstandard1.3 + .NET 4) spike.

---
+++ test/Blackstar.Tests/AssertionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public static class AssertionExtensions
+    {
+        public static void UriEqual(string expected, Uri actual)
+        {
+            Assert.Equal(expected, actual.ToString());
+        }
+    }
+}
---
+++ test/Blackstar.Tests/ClientTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using Xunit;
+using Blackstar;
+using System;
+
+namespace Tests
+{
+    public class ClientTests
+    {
+        readonly BlackstarClient _client = new BlackstarClient("http://demo.blackstarcms.net");
+
+        [Fact]
+        public void GetAll()
+        {
+            var chunks = _client.GetAllAsync().Result;
+            Assert.NotEmpty(chunks);
+            Assert.True(chunks.All(c => c.Id > 0));            
+            Assert.True(chunks.All(c => c.Value == ""));            
+        }
+
+        [Fact]
+        public void GetByTags()
+        {
+            var chunks = _client.GetByTagsAsync(new[] { "blackstarpedia" }).Result;
+            Assert.True(chunks.Length > 2);
+        }
+
+        [Fact]
+        public void GetByTag()
+        {
+            var chunks = _client.GetByTagAsync("blackstarpedia").Result;
+            Assert.True(chunks.Length > 2);
+        }
+
+        [Fact]
+        public void GetByIds()
+        {
+            var chunks = _client.GetByIdsAsync(new[] { 28,30,32 }).Result;
+            Assert.Equal(3, chunks.Length);
+        }
+
+        [Fact]
+        public void GetByNames()
+        {
+            var chunks = _client.GetByNamesAsync(new[] { "index-heading", "main-heading" }).Result;
+            Assert.Equal(2, chunks.Length);
+        }
+
+        [Fact]
+        public void NonExistingById()
+        {
+            var chunks = _client.GetByIdsAsync(new[] { 98374691 }).Result;
+            Assert.Empty(chunks);
+        }
+
+        [Fact]
+        public void WrongUrl()
+        {
+            Assert.Throws<AggregateException>(() => GetById()); 
+        }
+
+        private ContentChunk GetById() {
+            var _client = new BlackstarClient("http://wrong.blackstarcms.net");
+            return _client.GetByIdAsync(28).Result;
+        }
+    }
+}
